3d8fcf2f8ba11c75d69c967921e9f4b3bcd14208
WYT <as5818956@gmail.com>
2022-07-26 14:21:50 +0900

    Advance Rendering 01 flat & wireframe
    Advance Rendering 01 flat & wireframe

--- 2.10 Custom SRP/Assets/CustomRP/Runtime/CameraRenderer.cs
+++ 2.10 Custom SRP/Assets/CustomRP/Runtime/CameraRenderer.cs
@@ -7,19 +7,41 @@ namespace CustomRP.Runtime
     {
         private ScriptableRenderContext _context;
         private Camera _camera;
+        private const string _bufferName = "Render Camera";
+
+        private CommandBuffer _buffer = new CommandBuffer()
+        {
+            name = _bufferName
+        };
 
         public void Render(ScriptableRenderContext context, Camera camera)
         {
             _context = context;
             _camera = camera;
 
+            Setup();
             DrawVisibleGeometry();
             Submit();
         }
 
+        private void Setup()
+        {
+            _buffer.BeginSample(_bufferName);
+            ExecuteBuffer();
+            _context.SetupCameraProperties(_camera);
+        }
+
         private void Submit()
         {
+            _buffer.EndSample(_bufferName);
+            ExecuteBuffer();
             _context.Submit();
+        }
+
+        private void ExecuteBuffer()
+        {
+            _context.ExecuteCommandBuffer(_buffer);
+            _buffer.Clear();
         }
 
         private void DrawVisibleGeometry()
